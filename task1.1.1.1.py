7bad64c9f2ae78826bdf5e540cffca821cb33c7e
RibkaRibkina <RibkaRibkina1992@gmail.com>
2019-12-09 22:05:10 +0700

    Задачи на ветвления

---
+++ task1.1.1.1.py
@@ -0,0 +1,24 @@
+# Дано целое положительное число.
+# Напечатайте "чёт", если число чётное и "нечет", если число нечётное.
+# Пример ввода 1
+# 3
+# Пример вывода 1
+# нечет
+# Пример ввода 2
+# 4
+# Пример вывода 2
+# чёт
+
+def even_odd(number):
+    """
+    int -> str
+
+    checks the number for parity, and prints the answer.
+    """
+    if number % 2 == 0:
+        print("чёт")
+    else:
+        print("нечет")
+
+number = int(input("Введите число:"))
+even_odd(number)
---
+++ task2.2.2.2.py
@@ -0,0 +1,23 @@
+# Даны два целых числа.
+# Напечатайте меньшее из чисел.
+# Пример ввода
+# 3
+# 7
+# Пример вывода
+# 3
+
+def comparison(x, y):
+    """
+    (int, int) -> int
+
+    Compares numbers and returns the smaller.
+    """
+    if x > y:
+        return y
+    else:
+        y > x
+        return x
+
+number_x = int(input("Введите число: "))
+number_y = int(input("Введите число: "))
+print(comparison(number_x, number_y))
