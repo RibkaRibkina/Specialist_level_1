3234e58b6b88411a9f7e85b517a75c467f0cae7c
RibkaRibkina <RibkaRibkina1992@gmail.com>
2019-12-22 21:57:51 +0700

    Моудуль 4 закончен

--- Module4.py
+++ Module4.py
@@ -25,17 +25,103 @@
 # except:
 #     print("Ааааай!")
 # print("the end")
-try:
-    txt = input("Введите что-нибудь: ")
-    num = int(txt)
-except ValueError as e:
-    print(e)
-except (KeyboardInterrupt, EOFError) as e:
-    if type(e) == EOFError:
-        print("Вы сделали мне EOF")
-    else:
-        print("Отмена операции")
-    print("Вы отменили операцию!")
-else:
-    print("Вы ввели: " + txt)
-print("The end")
+# try:
+#     txt = input("Введите свой возраст: ")
+#     num = int(txt)
+# except ValueError as e:
+#     print(e)
+# except (KeyboardInterrupt, EOFError) as e:
+#     if type(e) == EOFError:
+#         print("Вы сделали мне EOF")
+#     else:
+#         print("Отмена операции")
+#     print("Вы отменили операцию!")
+# except:
+#     print("Что то случилось!")
+# else:
+#     print("Вы ввели: " + txt)
+# finally:
+#     print("The end")
+
+# try:
+#     age = input("Введите свой возраст: ")
+#     age = int(age)
+#     if age < 18:
+#         raise Exception("Не подходите по возрасту")
+#     print("Все хорошо!")
+# except ValueError as e:
+#     print(e)
+# except (KeyboardInterrupt, EOFError) as e:
+#     if type(e) == EOFError:
+#         print("Вы сделали мне EOF")
+#     else:
+#         print("Отмена операции")
+#     print("Вы отменили операцию!")
+# except Exception as e:
+#     print(e)
+# finally:
+#     print("The end")
+
+# try:
+#     n = 1
+#     try:
+#         s = "a" > 1
+#     except:
+#         print("inner")
+#         raise Exception("from inner")
+#     finally:
+#         print("ok")
+# except:
+#     print("outer")
+#     raise Exception("from inner")
+# finally:
+#     print("The end")
+
+# def one():
+#     s = "a" +"b"
+#     # raise Exception("")
+# def two():
+#     n = "a" > 1
+# def three():
+#     pass
+#
+# try:
+#     one()
+#     two()
+#     three()
+# except:
+#     print("Error")
+
+
+# def a():
+#     b()
+# def b():
+#     x()
+# def c():
+#     x()
+# def x():
+#     if True:
+#         raise Exception("Error")
+#
+# try:
+#     a()
+# except:
+#     print("!!!!")
+
+# from typing import List, Dict, Tuple
+#
+# s: str
+# n: int = 1
+# s = "a"
+#
+#
+# def is_equal(n1: int, n2: int) -> bool:
+#     return n1 == n2
+#
+#
+# x: int = input("")
+#
+# lst: List[int]
+# lst = [1, 2, 3]
+# d: Dict[str, int]
+# t: List[Tuple[int, int]]
